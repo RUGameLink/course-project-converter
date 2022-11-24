47528438f111abe0ebc82cdb6f32445d5901b2a0
Sad_Smiley <danilvovikov@gmail.com>
2022-11-25 00:09:23 +0800

    Интерфейс
    Описание графического интерфейса

--- eaToEbb/converterEKB/converterEKB/Form1.Designer.cs
+++ eaToEbb/converterEKB/converterEKB/Form1.Designer.cs
@@ -28,13 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.selectFileBtn = new System.Windows.Forms.Button();
+            this.selectPathBtn = new System.Windows.Forms.Button();
+            this.convertBtn = new System.Windows.Forms.Button();
+            this.convertProgress = new System.Windows.Forms.ProgressBar();
+            this.fileLabel = new System.Windows.Forms.Label();
+            this.pathLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // selectFileBtn
+            // 
+            this.selectFileBtn.Location = new System.Drawing.Point(42, 55);
+            this.selectFileBtn.Name = "selectFileBtn";
+            this.selectFileBtn.Size = new System.Drawing.Size(113, 53);
+            this.selectFileBtn.TabIndex = 0;
+            this.selectFileBtn.Text = "Загрузить файл";
+            this.selectFileBtn.UseVisualStyleBackColor = true;
+            // 
+            // selectPathBtn
+            // 
+            this.selectPathBtn.Location = new System.Drawing.Point(42, 144);
+            this.selectPathBtn.Name = "selectPathBtn";
+            this.selectPathBtn.Size = new System.Drawing.Size(113, 53);
+            this.selectPathBtn.TabIndex = 1;
+            this.selectPathBtn.Text = "Указать директорию \nдля сохранения";
+            this.selectPathBtn.UseVisualStyleBackColor = true;
+            // 
+            // convertBtn
+            // 
+            this.convertBtn.Location = new System.Drawing.Point(350, 321);
+            this.convertBtn.Name = "convertBtn";
+            this.convertBtn.Size = new System.Drawing.Size(113, 53);
+            this.convertBtn.TabIndex = 2;
+            this.convertBtn.Text = "Конвертировать файл";
+            this.convertBtn.UseVisualStyleBackColor = true;
+            // 
+            // convertProgress
+            // 
+            this.convertProgress.Location = new System.Drawing.Point(12, 240);
+            this.convertProgress.Name = "convertProgress";
+            this.convertProgress.Size = new System.Drawing.Size(776, 33);
+            this.convertProgress.TabIndex = 3;
+            // 
+            // fileLabel
+            // 
+            this.fileLabel.AutoSize = true;
+            this.fileLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(126)))), ((int)(((byte)(37)))));
+            this.fileLabel.Location = new System.Drawing.Point(162, 74);
+            this.fileLabel.Name = "fileLabel";
+            this.fileLabel.Size = new System.Drawing.Size(133, 13);
+            this.fileLabel.TabIndex = 4;
+            this.fileLabel.Text = "Файл успешно загружен";
+            // 
+            // pathLabel
+            // 
+            this.pathLabel.AutoSize = true;
+            this.pathLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.pathLabel.Location = new System.Drawing.Point(162, 164);
+            this.pathLabel.Name = "pathLabel";
+            this.pathLabel.Size = new System.Drawing.Size(149, 26);
+            this.pathLabel.TabIndex = 5;
+            this.pathLabel.Text = "Ошибка выбора \nдиректории для сохранения";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pathLabel);
+            this.Controls.Add(this.fileLabel);
+            this.Controls.Add(this.convertProgress);
+            this.Controls.Add(this.convertBtn);
+            this.Controls.Add(this.selectPathBtn);
+            this.Controls.Add(this.selectFileBtn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button selectFileBtn;
+        private System.Windows.Forms.Button selectPathBtn;
+        private System.Windows.Forms.Button convertBtn;
+        private System.Windows.Forms.ProgressBar convertProgress;
+        private System.Windows.Forms.Label fileLabel;
+        private System.Windows.Forms.Label pathLabel;
     }
 }
 
