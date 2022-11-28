f58b17427704383013b5c507ce51cce24b796239
Sad_Smiley <danilvovikov@gmail.com>
2022-11-29 03:23:39 +0800

    Много всего
    Заготовки проектов, дизайны, проектирование

---
+++ apps/rulemlToEkb/rulemlToEkb/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace rulemlToEkb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadFileBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileLbl = new System.Windows.Forms.Label();
+            this.pathLbl = new System.Windows.Forms.Label();
+            this.convertBtn = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(383, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadFileBtn,
+            this.saveFileBtn});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // loadFileBtn
+            // 
+            this.loadFileBtn.Name = "loadFileBtn";
+            this.loadFileBtn.Size = new System.Drawing.Size(225, 22);
+            this.loadFileBtn.Text = "Загрузить файл";
+            // 
+            // saveFileBtn
+            // 
+            this.saveFileBtn.Name = "saveFileBtn";
+            this.saveFileBtn.Size = new System.Drawing.Size(225, 22);
+            this.saveFileBtn.Text = "Выбрать место сохранения";
+            // 
+            // fileLbl
+            // 
+            this.fileLbl.AutoSize = true;
+            this.fileLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(126)))), ((int)(((byte)(37)))));
+            this.fileLbl.Location = new System.Drawing.Point(13, 82);
+            this.fileLbl.Name = "fileLbl";
+            this.fileLbl.Size = new System.Drawing.Size(87, 13);
+            this.fileLbl.TabIndex = 1;
+            this.fileLbl.Text = "Файл загружен";
+            // 
+            // pathLbl
+            // 
+            this.pathLbl.AutoSize = true;
+            this.pathLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.pathLbl.Location = new System.Drawing.Point(13, 113);
+            this.pathLbl.Name = "pathLbl";
+            this.pathLbl.Size = new System.Drawing.Size(125, 26);
+            this.pathLbl.TabIndex = 2;
+            this.pathLbl.Text = "Место для сохранения \nне установлено";
+            // 
+            // convertBtn
+            // 
+            this.convertBtn.Location = new System.Drawing.Point(110, 197);
+            this.convertBtn.Name = "convertBtn";
+            this.convertBtn.Size = new System.Drawing.Size(121, 67);
+            this.convertBtn.TabIndex = 3;
+            this.convertBtn.Text = "Конвертировать файл";
+            this.convertBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 326);
+            this.Controls.Add(this.convertBtn);
+            this.Controls.Add(this.pathLbl);
+            this.Controls.Add(this.fileLbl);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadFileBtn;
+        private System.Windows.Forms.ToolStripMenuItem saveFileBtn;
+        private System.Windows.Forms.Label fileLbl;
+        private System.Windows.Forms.Label pathLbl;
+        private System.Windows.Forms.Button convertBtn;
+    }
+}
+
---
+++ apps/swrlToEkb/swrlToEkb/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace swrlToEkb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.convertBtn = new System.Windows.Forms.Button();
+            this.fileLbl = new System.Windows.Forms.Label();
+            this.saveLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // fileBtn
+            // 
+            this.fileBtn.Location = new System.Drawing.Point(12, 12);
+            this.fileBtn.Name = "fileBtn";
+            this.fileBtn.Size = new System.Drawing.Size(112, 52);
+            this.fileBtn.TabIndex = 0;
+            this.fileBtn.Text = "Загрузить файл";
+            this.fileBtn.UseVisualStyleBackColor = true;
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(202, 12);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(112, 52);
+            this.saveBtn.TabIndex = 1;
+            this.saveBtn.Text = "Указать директорию \nдля сохранения";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            // 
+            // convertBtn
+            // 
+            this.convertBtn.Location = new System.Drawing.Point(101, 121);
+            this.convertBtn.Name = "convertBtn";
+            this.convertBtn.Size = new System.Drawing.Size(112, 52);
+            this.convertBtn.TabIndex = 2;
+            this.convertBtn.Text = "Конвертировать файл";
+            this.convertBtn.UseVisualStyleBackColor = true;
+            // 
+            // fileLbl
+            // 
+            this.fileLbl.AutoSize = true;
+            this.fileLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(126)))), ((int)(((byte)(37)))));
+            this.fileLbl.Location = new System.Drawing.Point(9, 67);
+            this.fileLbl.Name = "fileLbl";
+            this.fileLbl.Size = new System.Drawing.Size(133, 13);
+            this.fileLbl.TabIndex = 3;
+            this.fileLbl.Text = "Файл успешно загружен";
+            // 
+            // saveLbl
+            // 
+            this.saveLbl.AutoSize = true;
+            this.saveLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.saveLbl.Location = new System.Drawing.Point(179, 67);
+            this.saveLbl.Name = "saveLbl";
+            this.saveLbl.Size = new System.Drawing.Size(149, 26);
+            this.saveLbl.TabIndex = 4;
+            this.saveLbl.Text = "Ошибка выбора \nдиректории для сохранения";
+            this.saveLbl.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 239);
+            this.Controls.Add(this.saveLbl);
+            this.Controls.Add(this.fileLbl);
+            this.Controls.Add(this.convertBtn);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.fileBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button fileBtn;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.Button convertBtn;
+        private System.Windows.Forms.Label fileLbl;
+        private System.Windows.Forms.Label saveLbl;
+    }
+}
+
