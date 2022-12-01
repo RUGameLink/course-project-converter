41033ededb408fa2af559e9471ef742331aa2986
Sad_Smiley <danilvovikov@gmail.com>
2022-12-01 20:16:10 +0800

    Ангиутопия
    Работает считывание класса, делаю ассоциации

---
+++ apps/xmlReader/xmlReader/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace xmlReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textXml = new System.Windows.Forms.TextBox();
+            this.reeadBtn = new System.Windows.Forms.Button();
+            this.statusLbl = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // textXml
+            // 
+            this.textXml.Location = new System.Drawing.Point(12, 12);
+            this.textXml.Multiline = true;
+            this.textXml.Name = "textXml";
+            this.textXml.Size = new System.Drawing.Size(499, 426);
+            this.textXml.TabIndex = 0;
+            // 
+            // reeadBtn
+            // 
+            this.reeadBtn.Location = new System.Drawing.Point(635, 50);
+            this.reeadBtn.Name = "reeadBtn";
+            this.reeadBtn.Size = new System.Drawing.Size(75, 23);
+            this.reeadBtn.TabIndex = 1;
+            this.reeadBtn.Text = "Стартуем";
+            this.reeadBtn.UseVisualStyleBackColor = true;
+            this.reeadBtn.Click += new System.EventHandler(this.reeadBtn_Click);
+            // 
+            // statusLbl
+            // 
+            this.statusLbl.AutoSize = true;
+            this.statusLbl.Location = new System.Drawing.Point(536, 103);
+            this.statusLbl.Name = "statusLbl";
+            this.statusLbl.Size = new System.Drawing.Size(0, 13);
+            this.statusLbl.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusLbl);
+            this.Controls.Add(this.reeadBtn);
+            this.Controls.Add(this.textXml);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textXml;
+        private System.Windows.Forms.Button reeadBtn;
+        private System.Windows.Forms.Label statusLbl;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ apps/xmlReader/xmlReader/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace xmlReader
+{
+    public partial class Form1 : Form
+    {
+        string filename; //Путь к файлу
+        string test;
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.Filter = "Файл xml (*.xml)|*.xml|All files(*.*)|*.*";
+        }
+
+        private void reeadBtn_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+            {
+                statusLbl.Text = "Ошибка загрузки файла!";
+                statusLbl.ForeColor = Color.Red;
+                filename = null;
+            }
+            else
+            {
+                try
+                {
+                    string attr = "";
+                    string func = "";
+
+                    string start = "";
+                    string end = "";
+                    filename = openFileDialog1.FileName;
+                    textXml.Text = filename;
+                    XmlDocument xDoc = new XmlDocument();
+                    xDoc.Load(filename);
+                    XmlElement xRoot = xDoc.DocumentElement;
+                    if (xRoot != null)
+                    {
+                        var child = xRoot.ChildNodes;
+                        var content = child.Item(1);
+                        var model = content.FirstChild;
+                        var ownedElement = model.FirstChild;
+                        var packagesTemp = ownedElement.ChildNodes;
+                        var packages = packagesTemp.Item(1);
+                        var ownedElementTemp = packages.ChildNodes;
+                        var ownedElementClasses = ownedElementTemp.Item(1);
+                        var classes = ownedElementClasses.ChildNodes;
+                        for(int i = 0; i < classes.Count; i++)
+                        {
+                            var childClasses = classes.Item(i);
+                            if (childClasses.Name == "UML:Class")
+                            {
+                                var inside = childClasses.ChildNodes;
+                                var feature = inside.Item(1);
+                                var fields = feature.ChildNodes;
+                             //   MessageBox.Show(fields.ToString());
+                                for(int j = 0; j < fields.Count; j++)
+                                {
+                                    var operation = fields[j];
+                                    if(operation.Name == "UML:Attribute")
+                                    {
+                                        attr += $"\n{operation.Attributes["name"].Value} {operation.Attributes["visibility"].Value}";
+                                    //    MessageBox.Show($"{attr} {func}");
+                                    }
+                                    if(operation.Name == "UML:Operation")
+                                    {
+                                        func += $"\n{operation.Attributes["name"].Value} {operation.Attributes["visibility"].Value}";
+                                    //    MessageBox.Show($"{attr} {func}");
+                                    }
+                                 //   MessageBox.Show($"{attr} {func}");
+                                }
+                            //    MessageBox.Show("Я класс, сучка");
+                                var attribute = childClasses.Attributes["name"];
+                                if (attribute != null)
+                                {
+                                    string className = attribute.Value;
+                                    MessageBox.Show($"Я класс {className}" +
+                                        $"\nИмею поля {attr}" +
+                                        $"\nИ функции {func}");
+                                    // Process the value here
+                                }
+                                
+                            }
+                            else
+                            {
+                                //   MessageBox.Show("Ебана, я ассоциация...");
+                                var inside = childClasses.ChildNodes;
+                                var feature = inside.Item(0);
+
+
+                                if (feature.Attributes["tag"].Value == "ea_sourceName")
+                                {
+                                    start = feature.Attributes["value"].Value;
+                                }
+                                if (feature.Attributes["tag"].Value == "ea_targetName")
+                                {
+                                    end = feature.Attributes["value"].Value;
+                                }
+                                var attribute = childClasses.Attributes["name"];
+                                string assocName = attribute.Value;
+                               MessageBox.Show($"Я ассоциация {assocName}" +
+                                    $"\nИз класса {start}" +
+                                        $"\nВ класс {end}");
+                        // Process the value here
+                    
+                            }
+                            //MessageBox.Show(childClasses.Name);
+                            
+                        }
+                        
+                        Console.WriteLine("efefefef");
+                    }
+                    
+
+                }
+                catch {}
+            }
+        }
+    }
+}
